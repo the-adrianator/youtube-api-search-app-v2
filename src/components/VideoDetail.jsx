e0cf271ad7950682b103ecd3833db9c087e6a6da
CodeGenie.dev <adrian@codegenie.dev>
2025-09-08 20:33:14 +0100

    remove unused vars

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
-import { CheckCircle, SettingsInputSvideoSharp } from "@mui/icons-material";
+import { CheckCircle } from "@mui/icons-material";
 
 import { Videos } from './';
 import { fetchFromAPI } from "../utils/fetchFromAPI";
